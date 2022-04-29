f17b79f0f974a17b662e8bba218c59b1192302d5
moyoez <i@himoyo.cn>
2022-04-30 00:45:24 +0800

    加个driver

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/FloatTech/ZeroBot-Plugin-Playground
+
+go 1.18
+
+require (
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/tidwall/gjson v1.14.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/wdvxdr1123/ZeroBot v1.5.1 // indirect
+	golang.org/x/sys v0.0.0-20220429121018-84afa8d3f7b3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/driver"
+)
+
+func main() {
+	zero.OnCommand("hello").
+            Handle(func(ctx *zero.Ctx) {
+                ctx.Send("world")
+            })
+
+	zero.RunAndBlock(zero.Config{
+		NickName:      []string{"bot"},
+		CommandPrefix: "/",
+		SuperUsers:    []int64{123456},
+		Driver: []zero.Driver{
+			driver.NewWebSocketClient("ws://127.0.0.1:6700", "access_token"),
+		},
+	}, nil)
+}
