ab4262306be431ddd98a19d29ac4636f2c12e7a9
guohuiyuan <1156544355@qq.com>
2022-08-08 00:59:59 +0800

    ✨ 添加发音转换和ahsai

--- main.go
+++ main.go
@@ -11,11 +11,13 @@ import (
 	"github.com/FloatTech/zbputils/process"
 	"github.com/sirupsen/logrus"
 
+	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/ahsai"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/bilibilipush"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/breakrepeat"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/dailynews"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/example/JiangRed"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/example/xiaoguofan"
+	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/fyzhq"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/github/yydz"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/partygame"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/playwright"
