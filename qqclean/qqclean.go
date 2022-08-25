425c2f8532a90dd4a6ace2ef45b66e29ab5d30d8
himawari <1156544355@qq.com>
2022-08-25 11:42:06 +0800

    ✨ 添加自动拉群

--- qqclean/qqclean.go
+++ qqclean/qqclean.go
@@ -14,7 +14,7 @@ import (
 func init() {
 	engine := control.Register("qqclean", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
-		Help:             "qq号清理\n清理群聊@bot (清理bot不是管理员的群)\n",
+		Help:             "qq号清理\n清理群聊@bot (清理bot不是管理员的群)\n(当群聊数小于20时自动同意加群)\n",
 	})
 	engine.OnFullMatch("清理群聊", zero.SuperUserPermission, zero.OnlyToMe).SetBlock(true).Handle(func(ctx *zero.Ctx) {
 		cleanGroupnameList := make([]string, 0, 64)
@@ -26,5 +26,19 @@ func init() {
 			return true
 		})
 		ctx.SendPrivateMessage(ctx.Event.UserID, message.Text("已清理bot群聊: (", strings.Join(cleanGroupnameList, ", "), ")"))
+	})
+	engine.OnRequest().SetBlock(false).Handle(func(ctx *zero.Ctx) {
+		freecnt := 0
+		ctx.GetGroupList().ForEach(func(_, value gjson.Result) bool {
+			if ctx.GetGroupMemberInfo(value.Get("group_id").Int(), ctx.Event.SelfID, true).Get("role").String() == "member" {
+				freecnt++
+			}
+			return true
+		})
+		if freecnt < 20 {
+			ctx.SetGroupAddRequest(ctx.Event.Flag, "invite", true, "爱你哟")
+		} else {
+			ctx.SetGroupAddRequest(ctx.Event.Flag, "invite", false, "游离群聊大于20")
+		}
 	})
 }
