a0456be90b60fb834a0c212f5ec2c24099757068
喵酱 <51711558+nekohouse09@users.noreply.github.com>
2022-08-25 22:50:53 +0800

    Update tgyj.go

--- tgyj/tgyj.go
+++ tgyj/tgyj.go
@@ -22,7 +22,6 @@ func init() {
 	})
 	engine.OnRegex(`同归于尽.*?(\d+)`, zero.OnlyGroup).Limit(ctxext.LimitByGroup).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			rand.Seed(time.Now().Unix())                                     //种子
 			userban := rand.Intn(3) + 1                                      //生成群员禁言随机数 避免是0
 			me := userban + rand.Intn(2)                                     //生成自己禁言随机数
 			user := math.Str2Int64(ctx.State["regex_matched"].([]string)[1]) //字符串转整数
@@ -32,7 +31,7 @@ func init() {
 			ctx.SendChain(message.At(ctx.Event.UserID),
 				message.Text("\n你向"),
 				message.At(user),
-				message.Text("发动了 同归于尽 对方获得"+strconv.Itoa(int(userban))+"分钟禁言"+"  爆炸伤害波及到自己  自己获得"+strconv.Itoa(int(me))+"分钟禁言"),
+				message.Text("发动了 同归于尽 对方获得"+strconv.Itoa(userban)+"分钟禁言"+"  爆炸伤害波及到自己  自己获得"+strconv.Itoa(me)+"分钟禁言"),
 			)
 
 		})
