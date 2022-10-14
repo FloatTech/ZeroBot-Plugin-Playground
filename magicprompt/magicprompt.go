145dbc28843bc4c82daabcad4ed1eede521645f3
himawari <1156544355@qq.com>
2022-10-14 10:39:38 +0800

    ✨ 添加魔力提词

--- magicprompt/magicprompt.go
+++ magicprompt/magicprompt.go
@@ -2,18 +2,28 @@
 package magicprompt
 
 import (
-	"fmt"
+	"encoding/json"
 	"strings"
+	"time"
 
-	"github.com/FloatTech/floatbox/web"
 	ctrl "github.com/FloatTech/zbpctrl"
 	"github.com/FloatTech/zbputils/control"
+	"github.com/FloatTech/zbputils/ctxext"
+	"github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
 	"github.com/tidwall/gjson"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
-const magicpromptURL = "https://api-inference.huggingface.co/models/Gustavosta/MagicPrompt-Stable-Diffusion"
+const magicpromptURL = "wss://spaces.huggingface.tech/Gustavosta/MagicPrompt-Stable-Diffusion/queue/join"
+
+type hfRequest struct {
+	Action      string        `json:"action,omitempty"`
+	FnIndex     int           `json:"fn_index"`
+	Data        []interface{} `json:"data"`
+	SessionHash string        `json:"session_hash"`
+}
 
 func init() { // 插件主体
 	engine := control.Register("magicprompt", &ctrl.Options[*zero.Ctx]{
@@ -27,11 +37,62 @@ func init() { // 插件主体
 	engine.OnPrefix(`魔力提词`).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			args := ctx.State["args"].(string)
-			data, err := web.PostData(magicpromptURL, "application/json", strings.NewReader(fmt.Sprintf(`{"inputs": "%v"}`, args)))
+			c, _, err := websocket.DefaultDialer.Dial(magicpromptURL, nil)
 			if err != nil {
-				ctx.SendChain(message.Text("ERROR:", err))
+				ctx.SendChain(message.Text("Error:", err))
 				return
 			}
-			ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(gjson.ParseBytes(data).Get("@this.0.generated_text").String()))
+			defer c.Close()
+
+			done := make(chan struct{})
+
+			go func() {
+				defer close(done)
+				for {
+					_, m, err := c.ReadMessage()
+					if err != nil {
+						logrus.Println("read:", err)
+						return
+					}
+					text := gjson.ParseBytes(m).Get("output.data.0").String()
+					if text != "" {
+						m := message.Message{}
+						for _, v := range strings.Split(text, "\n\n") {
+							m = append(m, ctxext.FakeSenderForwardNode(ctx, message.Text(v)))
+						}
+						if id := ctx.Send(m).ID(); id == 0 {
+							ctx.SendChain(message.Text("ERROR: 可能被风控或下载图片用时过长，请耐心等待"))
+						}
+					}
+					logrus.Printf("recv: %s", m)
+				}
+			}()
+
+			r := hfRequest{
+				FnIndex:     0,
+				Data:        []interface{}{args},
+				SessionHash: "zerobot",
+			}
+			b, err := json.Marshal(r)
+			if err != nil {
+				ctx.SendChain(message.Text("Error:", err))
+				return
+			}
+
+			ticker := time.NewTicker(time.Second)
+			defer ticker.Stop()
+
+			for {
+				select {
+				case <-done:
+					return
+				case <-ticker.C:
+					err := c.WriteMessage(websocket.TextMessage, b)
+					if err != nil {
+						ctx.SendChain(message.Text("Error:", err))
+						return
+					}
+				}
+			}
 		})
 }
