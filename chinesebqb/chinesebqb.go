bf1574febcccf0b99cdbfb5d4d8cd5a57c86f0b8
himawari <1156544355@qq.com>
2022-11-18 16:12:59 +0800

    ✨ 添加chinesebqb表情包

---
+++ chinesebqb/chinesebqb.go
@@ -0,0 +1,58 @@
+// Package chinesebqb 表情包
+package chinesebqb
+
+import (
+	"math/rand"
+
+	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/control"
+	"github.com/FloatTech/zbputils/ctxext"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+
+func init() { // 插件主体
+	engine := control.Register("chinesebqb", &ctrl.Options[*zero.Ctx]{
+		DisableOnDefault: false,
+		Help: "chinesebqb表情包\n" +
+			"- 表情包",
+		PrivateDataFolder: "chinesebqb",
+	})
+	go func() {
+		bdb = initialize(engine.DataFolder() + "chinesebqb.db")
+	}()
+	engine.OnPrefix(`表情包`).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			ctx.SendChain(message.Text("少女祈祷中..."))
+			args := ctx.State["args"].(string)
+			blist, err := bdb.getByKey(args)
+			if err != nil {
+				ctx.SendChain(message.Text("ERROR: ", err))
+				return
+			}
+			imageList := make([]string, 0, 64)
+			for _, v := range blist {
+				imageList = append(imageList, v.URL)
+			}
+			rand.Shuffle(len(imageList), func(i, j int) {
+				imageList[i], imageList[j] = imageList[j], imageList[i]
+			})
+			m := message.Message{}
+			for _, v := range imageList[:5] {
+				m = append(m, ctxext.FakeSenderForwardNode(ctx, message.Image(v)))
+			}
+			if id := ctx.Send(m).ID(); id == 0 {
+				ctx.SendChain(message.Text("ERROR: 可能被风控或下载图片用时过长，请耐心等待"))
+			}
+		})
+	engine.OnFullMatch(`更新表情包`, zero.SuperUserPermission).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			ctx.SendChain(message.Text("少女祈祷中..."))
+			err := bdb.truncateAndInsert()
+			if err != nil {
+				ctx.SendChain(message.Text("ERROR: ", err))
+				return
+			}
+			ctx.SendChain(message.Text("chinesebqb表情包更新完毕"))
+		})
+}
--- go.mod
+++ go.mod
@@ -8,11 +8,10 @@ require (
 	github.com/FloatTech/sqlite v0.4.0
 	github.com/FloatTech/zbpctrl v1.5.2-0.20220921013146-40d64bc7799c
 	github.com/FloatTech/zbputils v1.5.1-0.20221004094248-ad5a9235757e
-	github.com/fumiama/sqlite3 v1.14.6
+	github.com/glebarez/sqlite v1.5.0
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/guohuiyuan/go-fyzhq v0.0.0-20220807135412-25c16c843e5a
-	github.com/jinzhu/gorm v1.9.16
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/playwright-community/playwright-go v0.2000.1
 	github.com/sirupsen/logrus v1.9.0
@@ -20,6 +19,7 @@ require (
 	github.com/wcharczuk/go-chart/v2 v2.1.0
 	github.com/wdvxdr1123/ZeroBot v1.6.3
 	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539
+	gorm.io/gorm v1.24.1
 )
 
 require (
@@ -33,9 +33,12 @@ require (
 	github.com/fumiama/go-base16384 v1.6.1 // indirect
 	github.com/fumiama/go-registry v0.1.6 // indirect
 	github.com/fumiama/gofastTEA v0.0.10 // indirect
+	github.com/fumiama/sqlite3 v1.14.6 // indirect
+	github.com/glebarez/go-sqlite v1.19.1 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mozillazg/go-pinyin v0.19.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
@@ -48,4 +51,5 @@ require (
 	modernc.org/libc v1.19.0 // indirect
 	modernc.org/mathutil v1.5.0 // indirect
 	modernc.org/memory v1.4.0 // indirect
+	modernc.org/sqlite v1.19.1 // indirect
 )
