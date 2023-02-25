5b4f957feffdd6fe94786e0ad1b003f061794543
fireworkmarks <806229693@qq.com>
2023-02-25 14:50:46 +0800

    feature:CR fix

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/qqci"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/qqclean"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/recall" // SGK2401
-	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/steamstatus"
+	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/steam"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/subweibo"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/tgyj"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/vote"
