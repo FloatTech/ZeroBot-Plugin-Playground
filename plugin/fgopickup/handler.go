d6a1b616dea15ce56d82da3ade192151f6459ed6
Arcueid <1170895958@qq.com>
2023-03-24 17:15:58 +0800

    feat: 一次合并发送全部卡池

--- plugin/fgopickup/handler.go
+++ plugin/fgopickup/handler.go
@@ -13,22 +13,15 @@ func listPickups(ctx *zero.Ctx) {
 	pickups := service.getPickups()
 
 	msg := message.Message{}
-	for index, pickup := range pickups {
+	for _, pickup := range pickups {
 		id := message.Text("id:" + strconv.Itoa(pickup.Id) + "\n")
 		banner := message.Image(pickup.Banner)
 		name := message.Text("\n" + pickup.Name)
 		date := message.Text("\n" +
 			parseTime(pickup.StartTime) + "~" + parseTime(pickup.EndTime))
 		msg = append(msg, ctxext.FakeSenderForwardNode(ctx, id, banner, name, date))
-		if (index+1)%20 == 0 {
-			ctx.Send(msg)
-			msg = message.Message{}
-		}
 	}
-
-	if len(msg) > 0 {
-		ctx.Send(msg)
-	}
+	ctx.Send(msg)
 }
 
 func parseTime(timeInSeconds int64) string {
