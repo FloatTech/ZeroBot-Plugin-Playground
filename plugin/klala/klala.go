54e93483cb74a7fbc95642e7cd7014bda0343e81
lianhong <2758988938@qq.com>
2023-04-27 21:48:00 +0800

    add 全部指令查询

--- plugin/klala/klala.go
+++ plugin/klala/klala.go
@@ -5,10 +5,12 @@ import (
 	"encoding/json"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/FloatTech/floatbox/file"
 	ctrl "github.com/FloatTech/zbpctrl"
 	"github.com/FloatTech/zbputils/control"
+	"github.com/FloatTech/zbputils/ctxext"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
 	"github.com/wdvxdr1123/ZeroBot/utils/helper"
@@ -18,7 +20,7 @@ func init() { // 主函数
 	en := control.Register("klala", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
 		Brief:            "星穹铁道图鉴查询",
-		Help: "- 更新图鉴\n" +
+		Help: "- *更新图鉴\n" +
 			"- *xx图鉴",
 		PrivateDataFolder: "klala",
 	})
@@ -50,7 +52,7 @@ func init() { // 主函数
 		}
 		ctx.SendChain(message.Image("file:///" + file.BOTPATH + "/" + en.DataFolder() + "star-rail-atlas" + path))
 	})
-	en.OnRegex(`^更新图鉴$`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
+	en.OnRegex(`^*更新图鉴$`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
 		var cmd *exec.Cmd
 		if file.IsNotExist(en.DataFolder() + "star-rail-atlas") {
 			cmd = exec.Command("git", "clone", "https://github.com/Nwflower/star-rail-atlas.git")
@@ -64,6 +66,32 @@ func init() { // 主函数
 			return
 		}
 		ctx.SendChain(message.Text("运行成功: ", helper.BytesToString(output)))
+	})
+	en.OnRegex(`^*图鉴列表$`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
+		if file.IsNotExist(en.DataFolder() + "star-rail-atlas") {
+			ctx.SendChain(message.Text("请先发送\"更新图鉴\"!"))
+			return
+		}
+		t, err := os.ReadFile(en.DataFolder() + "star-rail-atlas/path.json") // 获取文件
+		if err != nil {
+			ctx.SendChain(message.Text("获取路径文件失败", err))
+			return
+		}
+		var paths wikimap
+		_ = json.Unmarshal(t, &paths)
+		var msg1, msg2 strings.Builder
+		msg1.WriteString("lightcone: \n")
+		for i := range paths.Light {
+			msg1.WriteString(i)
+			msg1.WriteString("\n")
+		}
+		msg2.WriteString("role: \n")
+		for i := range paths.Role {
+			msg2.WriteString(i)
+			msg2.WriteString("\n")
+		}
+		ctx.Send(message.Message{ctxext.FakeSenderForwardNode(ctx, message.Text(msg1.String())),
+			ctxext.FakeSenderForwardNode(ctx, message.Text(msg2.String()))})
 	})
 }
 
