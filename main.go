f224b825f2c25c9fec5f1c52b44986a6cf739c7e
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2023-05-06 18:46:48 +0800

    makelinthappy (#213)

--- main.go
+++ main.go
@@ -30,13 +30,9 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/fgopickup"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/fyzhq"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/games"
-	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/klala" //星穹铁道图鉴
+	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/klala" // 星穹铁道图鉴
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/kokomi"
 
-	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/vtbwife"// 抽vtbwife
-	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/arcaea"
-	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/draw" // 服务详情
-	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/fgopickup"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/moegozh"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/movies"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/ottoshout"
@@ -49,7 +45,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/subweibo"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/tgyj"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/vote"
-	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/vtbwife" //抽vtbwife
+	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/vtbwife" // 抽vtbwife
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/wantquotes"
 	_ "github.com/FloatTech/ZeroBot-Plugin-Playground/plugin/youdaotranslate"
 
