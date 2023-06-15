fa96787aa158c33f9fc0f4bb1b234953028feafe
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2023-06-15 20:41:17 +0800

    Fixlint (#221)
    * test
    
    * test
    
    * 1.20
    
    * make lint happy

--- main.go
+++ main.go
@@ -153,7 +153,7 @@ func init() {
 }
 func main() {
 	if !strings.Contains(runtime.Version(), "go1.2") { // go1.20之前版本需要全局 seed，其他插件无需再 seed
-		rand.Seed(time.Now().UnixNano())
+		rand.Seed(time.Now().UnixNano()) //nolint:staticcheck
 	}
 
 	zero.OnCommand("hello").
