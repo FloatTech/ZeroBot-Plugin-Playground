1a704901e6db71bb7f495e75e58a8964d5299888
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2023-06-15 12:40:27 +0000

    make lint happy

--- plugin/klala/net.go
+++ plugin/klala/net.go
@@ -3,7 +3,7 @@ package klala
 import (
 	"encoding/base64"
 	"errors"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 	"strconv"
@@ -44,7 +44,7 @@ func getRole(uid string) (body []byte, err error) {
 	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusBadRequest {
 		return nil, errors.New("获取数据失败, Code: " + strconv.Itoa(res.StatusCode))
 	}
-	body, err = ioutil.ReadAll(res.Body)
+	body, err = io.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
